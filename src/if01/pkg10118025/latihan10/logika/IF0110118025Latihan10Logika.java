b43ed5d84400f36055098b3464f070d59681669e
rizki2704 <55817452+rizki2704@users.noreply.github.com>
2019-09-26 08:09:29 +0700

    Add files via upload

---
+++ src/if01/pkg10118025/latihan10/logika/IF0110118025Latihan10Logika.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pkg10118025.latihan10.logika;
+
+/**
+ *
+ * @author
+ * Nama     : Rizki Restu Illahi
+ * Kelas    : IF01
+ * NIM      : 10118025
+ */
+public class IF0110118025Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
